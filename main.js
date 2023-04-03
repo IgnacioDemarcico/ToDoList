89bd19ac8a65e7c5c638ba8518a0bb3122b86219
santiagokasses <101513911+santiagokasses@users.noreply.github.com>
2023-04-02 23:10:33 -0300

    FUNCIONA TODOOOOO

--- main.js
+++ main.js
@@ -2,46 +2,58 @@ const mostrarArray = arr => console.log(`Array:${arr.map(e => " " + e)}`)
 
 
 var notas = 0
+var nota
 
 const arrayToDo = []
 const arrayGuardado = []
 const arrayTildado = []
 
 const añadir = (clear = true) => {
+nota = document.getElementById("nota").value
+arrayToDo.push(document.getElementById("nota").value)
 document.getElementById("chekbox").innerHTML += `
-<li class="list-group-item element" id="lista-${notas}">
+<li class="fecha list-group-item element " id="lista-${notas}">
 <div class="content">
-<button type="button" class="btn btn-primary" data-bs-toggle="button" onclick="tachar()">Toggle button</button>
+<button type="button" class="btn btn-primary" data-bs-toggle="button" onclick="tachar(${notas})">Toggle button</button>
 <label id="tachar" class="form-check-label" for="firstCheckbox">${document.getElementById("nota").value}</label>
 </div>
 <button type="button" class="btn btn-danger" onclick="eliminar(${notas})">Eliminar</button>
 </li>
 `
-
 if(clear) document.getElementById("nota").value = ""
 notas++
 
 
 
-
 //const time = document.getElementById("time")
 //time.firstChild.nodeValue = event.timeStamp
-arrayToDo.push(document.getElementById("nota").value)
+
 arrayGuardado.push(event.timeStamp)
 mostrarArray(arrayToDo)
 mostrarArray(arrayGuardado)
 }
 
-function tachar() {
-    document.getElementById("tachar").innerHTML += `hola`
-    arrayTildado.push(event.timeStamp)
+function tachar(id) {
+    let elementoModificado = document.querySelector(`#lista-${id} > div > label`)
+    if(elementoModificado.innerHTML != `<strike> ${nota} </strike>`){
+        elementoModificado.innerHTML = `<strike> ${nota} </strike>`
+        document.getElementsByClassName("fecha").className += "time-${event.timeStamp}"
+        arrayTildado.push(event.timeStamp)
+    }
+    else if(elementoModificado.innerHTML = `<strike> ${nota} </strike>`){
+        elementoModificado.innerHTML = `${nota}`
+        arrayTildado.splice(document.getElementById(`lista-${id}`),1)
+    }
     mostrarArray(arrayTildado)
 }
 
 
+function eliminar(id){ 
+    document.getElementById(`lista-${id}`).remove()
+    arrayTildado.splice(document.getElementById(`lista-${id}`),1)
+    arrayToDo.splice(document.getElementById(`lista-${id}`),1)
+}
 
-
-const eliminar = id => document.getElementById(`lista-${id}`).remove()
 
 
 
